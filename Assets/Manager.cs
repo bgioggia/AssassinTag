7737d6b9db1223b3944385817e84094650c5dad0
Brandon Gioggia <bgioggia@gmail.com>
2021-04-23 22:08:09 -0400

    -Orbs now start with an assassin and a protector. These can be displayed in color by clicking on an orb highlighting it blue, its assassin red, and its protector green.

---
+++ Assets/Manager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public GameObject prefabOrb;
+    private List<GameObject> _orbsList;
+    private int _cols, _rows;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cols = 4;
+        _rows = 4;
+        _orbsList = PlaceOrbs();
+        AssignRelationships();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    List<GameObject> PlaceOrbs()
+    {
+        List<GameObject> orbs = new List<GameObject>();
+        for (int columns = 0; columns < _cols; columns++)
+        {
+            for (int rows = 0; rows < _rows; rows++)
+            {
+                var new_orb = Instantiate(prefabOrb);
+                new_orb.transform.position = new Vector2(columns-8, rows - 4);
+                var orbID = orbs.Count + 1;
+                new_orb.GetComponent<Orb>().AssignManager(gameObject);
+                new_orb.GetComponent<Orb>().AssignOrbID(orbID);
+                orbs.Add(new_orb);
+            }
+        }
+        return orbs;
+    }
+
+    void AssignRelationships()
+    {
+        for (int i = 0; i < _orbsList.Count; i++)
+        {
+            var protectorNum = 0;
+            var assassinNum = 0;
+            while (protectorNum == assassinNum || assassinNum == i || protectorNum == i)
+            { 
+                protectorNum = Random.Range(0, _orbsList.Count);
+                assassinNum = Random.Range(0, _orbsList.Count);
+            }
+
+            var assassin = _orbsList[assassinNum];
+            var protector = _orbsList[protectorNum];
+            var mainCharacter = _orbsList[i];
+            
+            mainCharacter.GetComponent<Orb>().AssignAssassin(assassin);
+            mainCharacter.GetComponent<Orb>().AssignProtector(protector);
+        }
+    }
+
+    public bool clearOrbColors()
+    {
+        foreach (var orb in _orbsList)
+        {
+            orb.GetComponent<Orb>().SetHoiPaloiColor();
+        }
+        return true;
+    }
+}
---
+++ Assets/Orb.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orb : MonoBehaviour
+{
+    private static readonly int ShaderColor = Shader.PropertyToID("_Color");
+    private GameObject _assassin, _protector, _manager;
+    private int _orbID;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+
+    public void AssignAssassin(GameObject orb)
+    {
+        _assassin = orb;
+    }
+
+    public void AssignProtector(GameObject orb)
+    {
+        _protector = orb;
+    }
+
+    public void AssignManager(GameObject manager)
+    {
+        _manager = manager;
+    }
+    
+    public void AssignOrbID(int id)
+    {
+        _orbID = id;
+    }
+    
+    private void OnMouseDown()
+    {
+        if (!_manager.GetComponent<Manager>().clearOrbColors()) return;
+        SetMainCharacterColor();
+        _assassin.GetComponent<Orb>().SetAssassinColor();
+        _protector.GetComponent<Orb>().SetProtectorColor();
+    }
+
+    public void SetMainCharacterColor()
+    {
+        gameObject.GetComponent<Renderer>().material.SetColor(ShaderColor, Color.blue);
+
+    }
+
+    public void SetProtectorColor()
+    {
+        gameObject.GetComponent<Renderer>().material.SetColor(ShaderColor, Color.green);
+    }
+
+    public void SetAssassinColor()
+    {
+        gameObject.GetComponent<Renderer>().material.SetColor(ShaderColor, Color.red);
+    }
+
+    public void SetHoiPaloiColor()
+    {
+        gameObject.GetComponent<Renderer>().material.SetColor(ShaderColor, Color.white);
+
+    }
+}
